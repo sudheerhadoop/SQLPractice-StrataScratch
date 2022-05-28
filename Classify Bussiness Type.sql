6e40dfc64fb3a934df7fc7fe74da88f247effac8
sudheerhadoop <sudhe9630@gmail.com>
2022-05-28 11:15:37 +0530

    Create Classify Bussiness Type.sql

---
+++ Classify Bussiness Type.sql
@@ -0,0 +1,6 @@
+select distinct business_name, 
+case when lower(business_name) like '%restaurant%' then 'restaurant'
+        when lower(business_name) like '%cafe%' or lower(business_name) like '%café' or lower(business_name) like '%coffee%' then 'cafe'
+        when lower(business_name) like '%school%' then 'school'
+        else 'other' end  business_type
+     from sf_restaurant_health_violations;
