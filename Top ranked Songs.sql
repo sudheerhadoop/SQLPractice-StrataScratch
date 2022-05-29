2e782b2e06b5be6c0ed417483d8d1380b4b022b5
sudheerhadoop <sudhe9630@gmail.com>
2022-05-29 12:18:49 +0530

    Create Top ranked Songs.sql

---
+++ Top ranked Songs.sql
@@ -0,0 +1,10 @@
+select 
+    trackname, 
+    count(*) as times_top1 
+from spotify_worldwide_daily_song_ranking
+where 
+    position = 1
+group by 
+    trackname
+order by 
+    times_top1 DESC;
