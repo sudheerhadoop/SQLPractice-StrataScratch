4afe43e5cde195e83659511cfd4982722bc275aa
sudheerhadoop <sudhe9630@gmail.com>
2022-05-30 15:59:45 +0530

    Update Ranking Most active guests.sql

--- Ranking Most active guests.sql
+++ Ranking Most active guests.sql
@@ -16,3 +16,16 @@ from
     sum_n_messages,
     dense_rank() over(order by sum_n_messages desc) rn
 from messages_sum) inn;
+
+
+
+
+/**********************************/
+
+SELECT 
+    DENSE_RANK() OVER(ORDER BY sum(n_messages) DESC) as ranking, 
+    id_guest, 
+    sum(n_messages) as sum_n_messages
+FROM airbnb_contacts
+GROUP BY id_guest
+ORDER BY sum_n_messages DESC
